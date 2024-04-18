4a1cd0b58a6315e546b5534dd292a89a58d64c5d
Guarav Khaire <gauravgkhaire@gmail.com>
2024-04-18 17:46:57 +0530

    upadate

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -85,8 +85,9 @@ export function Footer() {
           variant="small"
           className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
         >
-          &copy; {currentYear} <a href="https://criconai.com">Cricon Ai</a>. All
-          Rights Reserved.
+          &copy; {currentYear}
+          <a href="https://criconai.com">GK Services(Cricon Ai)</a>. All Rights
+          Reserved.
         </Typography>
         <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
           <Typography
