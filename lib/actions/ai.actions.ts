81e0be66f483d55e8ad7918469fa6d79909337fd
Guarav Khaire <gauravgkhaire@gmail.com>
2024-04-19 19:25:04 +0530

    now

--- lib/actions/ai.actions.ts
+++ lib/actions/ai.actions.ts
@@ -231,10 +231,7 @@ export const generateGptResponse = async ({
       // });
 
       // const speechBuffer = Buffer.from(await mp3.arrayBuffer());
-      const outputFile = path.join(
-        process.cwd(),
-        "public/assets/audio/output.mp3"
-      );
+      const outputFile = path.resolve("public/assets/audio/output.mp3");
 
       // await fs.writeFileSync(outputFile, speechBuffer);
 
