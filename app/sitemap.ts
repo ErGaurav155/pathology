3941a63fe5991d79a7f61d73df781e90af8680a6
Guarav Khaire <gauravgkhaire@gmail.com>
2024-04-26 18:18:56 +0530

    now

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,31 +1,21 @@
 import { MetadataRoute } from "next";
 
-export default function sitemap(params: { type: any }): MetadataRoute.Sitemap {
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://criconai.com",
+      url: "https://criconai.com/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
-    {
-      url: "https://criconai.com/credits",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.5,
-    },
+
     {
       url: "https://criconai.com/contactUs",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
     },
-    {
-      url: "https://criconai.com/profile",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
+
     {
       url: "https://criconai.com/TermsandCondition",
       lastModified: new Date(),
@@ -34,40 +24,6 @@ export default function sitemap(params: { type: any }): MetadataRoute.Sitemap {
     },
     {
       url: "https://criconai.com/privacy-policy",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-
-    {
-      url: `https://criconai.com/criconai/contentwriter/${params}`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-    {
-      url: `https://criconai.com/criconai/longvid/${params}`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-
-    {
-      url: `https://criconai.com/criconai/shortvid/${params}`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-
-    {
-      url: `https://criconai.com/criconai/socialmedia/${params}`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-
-    {
-      url: `https://criconai.com/criconai/marketing/${params}`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.9,
