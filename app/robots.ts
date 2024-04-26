6501a6e17302696c7f21a2dce0fee7450c865a2d
Guarav Khaire <gauravgkhaire@gmail.com>
2024-04-26 18:30:52 +0530

    allow

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "/private/",
     },
     sitemap: "https://criconai.com/sitemap.xml",
   };
