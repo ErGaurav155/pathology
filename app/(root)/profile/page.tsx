0a68834e5ad1880afb2ccfdc2e7258cb00886d6c
Guarav Khaire <gauravgkhaire@gmail.com>
2024-05-05 13:48:58 +0530

    now

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,44 +1,18 @@
-"use client";
-
 import { auth } from "@clerk/nextjs";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
 import Header from "@/components/shared/Header";
-import { getUserByDbId, getUserById } from "@/lib/actions/user.actions";
+import { getUserById } from "@/lib/actions/user.actions";
 import Link from "next/link";
-import Heading from "@/components/shared/Svgs/HEading";
-import { DownloadIcon } from "lucide-react";
-import { download } from "@/lib/utils";
-import { useEffect, useState } from "react";
 
-export default function Profile() {
-  const [userData, setUserData] = useState<any>(null);
+const Profile = async () => {
+  const { userId } = auth();
 
-  useEffect(() => {
-    // Fetch user data when component mounts
-    async function fetchData() {
-      try {
-        const { userId } = auth();
+  if (!userId) redirect("/sign-in");
 
-        if (!userId) redirect("/sign-in");
-        const user = await getUserById(userId);
-        setUserData(user);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    }
-
-    fetchData();
-  }, []);
-  const downloadHandler = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    item: string,
-    title: string
-  ) => {
-    e.preventDefault();
-    download(item, title);
-  };
+  const user = await getUserById(userId);
+  const userID = user._id;
   return (
     <div className="wrapper">
       <Header title="Profile" />
@@ -54,7 +28,7 @@ export default function Profile() {
               height={50}
               className="size-9 md:size-12"
             />
-            <h2 className="h2-bold text-dark-600">{userData.creditBalance}</h2>
+            <h2 className="h2-bold text-dark-600">{user.creditBalance}</h2>
           </div>
         </div>
 
@@ -68,51 +42,14 @@ export default function Profile() {
               height={50}
               className="size-9 md:size-12"
             />
-            <h2 className="h2-bold text-dark-600">
-              {userData.imageUrls.length}
-            </h2>
+            <Link href={`/criconai/profile/${userID}`}>
+              <h2 className="h2-bold text-dark-600">{user.imageUrls.length}</h2>
+            </Link>
           </div>
-        </div>
-        <div className="p-4 mt-5  ">
-          <Heading title="Image Gallery" />
-          {userData && userData.imageUrls && userData.imageUrls.length > 0 ? (
-            <div className="grid p-0 sm:p-[80px] md:p-0  md:grid-cols-2 xl:grid-cols-3 gap-3 sm:gap-4 md:gap-5 lg:gap-6 mb-0 ">
-              {userData.imageUrls.map((imageUrl: string, index: number) => (
-                <div
-                  className={`rounded-md overflow-hidden relative w-[${1024}]
-        h-[${1024}]`}
-                  key={index}
-                >
-                  <button
-                    className="absolute top-1 right-1 rounded-md bg-white p-2"
-                    onClick={(e) =>
-                      downloadHandler(
-                        e,
-                        imageUrl,
-                        "image" +
-                          (Math.floor(Math.random() * 100) + 1).toString()
-                      )
-                    }
-                  >
-                    <DownloadIcon />
-                  </button>
-
-                  <Image
-                    alt="image"
-                    className="flex-1 "
-                    src={imageUrl}
-                    width={1024}
-                    height={1024}
-                    priority
-                  />
-                </div>
-              ))}
-            </div>
-          ) : (
-            <Heading title="No images found." />
-          )}
         </div>
       </section>
     </div>
   );
-}
+};
+
+export default Profile;
